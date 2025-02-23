a91c8b9aa2790f60b90f6a3b6f2968ec97b4312d
ShivaniMane <shivanimane43@gmail.com>
2025-02-23 13:34:51 +0530

    Add two subsections to Features and create a reusable SectionTitle component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,34 +2,47 @@ import React from "react";
 import FeatureWrapper from "./FeatureWrapper";
 import SectionTitle from "./SectionTitle";
 import avatar from "../assets/avatar.svg";
-import LogoWrapper from "./LogoWrapper";
 import setting_lg from "../assets/setting-lg.svg";
 import setting_sm from "../assets/setting-sm.svg";
 import pie_chart from "../assets/piechart.svg";
+import flash from "../assets/flash.svg";
 const Features = () => {
   return (
     <div className="flex flex-col mx-auto">
-      <SectionTitle />
+      <SectionTitle
+        sectionName="FEATURES"
+        sectionTitle="Feature packed to make "
+        sectionTitleSapn="Digital marketing easier and affordable."
+        setionDesc="Experience Intelligent Features to optimize your marketing efforts."
+      />
       <div className="flex flex-col desktop:grid-cols-3 sm:grid sm:grid-cols-2 gap-7 mx-auto mt-10">
         <FeatureWrapper>
           <div className="relative grid grid-flow-row justify-center max-h-full py-24">
             <div className="absolute w-96 h-96 border rounded-full border-gray-600 top-14 -left-4 opacity-30 animate-ping ease-in-out bg-slate-200"></div>
             <div className="absolute w-96 h-96 border rounded-full border-gray-600 top-14 -left-4 opacity-10"></div>
             <div className="absolute w-64 h-64 rounded-full border-gray-600 top-28 left-12 flex items-center justify-center overflow-hidden">
-              <span className="absolute w-3 h-3 bg-slate-300 rounded-full z-10"></span>
+              <span className="absolute w-3 h-3 bg-slate-300 rounded-full"></span>
               <span className="absolute w-32 h-32 border-gray-600 rounded-full animate-ping border"></span>
               <img
                 src={avatar}
                 alt="avatar"
-                className="absolute bottom-6 appear"
+                className="absolute animate-pulse  bottom-6"
               />
               <img
                 src={avatar}
                 alt="avatar"
-                className="absolute animate-pulse top-6"
+                className="absolute animate-pulse  top-6"
               />
-              <img src={avatar} alt="avatar" className="absolute right-6" />
-              <img src={avatar} alt="avatar" className="absolute left-6" />
+              <img
+                src={avatar}
+                alt="avatar"
+                className="absolute animate-pulse duration-100 right-6"
+              />
+              <img
+                src={avatar}
+                alt="avatar"
+                className="absolute animate-pulse delay-75 left-6"
+              />
               <div className="animate-rotate absolute inset-0 h-full w-full rounded-full bg-[conic-gradient(#333B4F_20deg,transparent_50deg)] opacity-50"></div>
             </div>
           </div>
@@ -54,7 +67,7 @@ const Features = () => {
                     <img
                       src={setting_lg}
                       alt=" setting-lg"
-                      className="animate-rotate w-2 h-2 "
+                      className="animate-rotate w-2 h-2"
                     />
                     <img
                       src={setting_sm}
@@ -84,7 +97,7 @@ const Features = () => {
                 </div>
               </div>
               <div>
-                <img src={pie_chart} alt="" />
+                <img src={pie_chart} alt="Pie-chart" />
               </div>
             </div>
             <div className="absolute bottom-0 w-full px-5 py-2 space-y-1 ">
@@ -99,7 +112,47 @@ const Features = () => {
             </div>
           </FeatureWrapper>
         </div>
-        <FeatureWrapper></FeatureWrapper>{" "}
+        <FeatureWrapper>
+          <div className="relative grid grid-flow-row justify-center max-h-full py-24">
+            <div className="absolute w-96 h-96 border rounded-full border-gray-200 top-14 -left-4 opacity-10"></div>
+            <div className="absolute w-64 h-64 rounded-full border border-gray-700 top-28 left-12 flex items-center justify-center"></div>
+            <div className="absolute w-64 h-64 rounded-full border-gray-600 top-28 left-12 flex items-center justify-center ">
+              <img src={flash} alt="flash" className="z-10" />
+            </div>
+            <div className="absolute w-36 h-36 rounded-full top-40 left-24 flex items-center justify-center animate-pingSlow">
+              <img
+                src={avatar}
+                alt="avatar"
+                className="absolute -bottom-6 w-3 h-3"
+              />
+              <img
+                src={avatar}
+                alt="avatar"
+                className="absolute  -top-6 w-3 h-3"
+              />
+              <img
+                src={avatar}
+                alt="avatar"
+                className="absolute -right-6 w-3 h-3"
+              />
+              <img
+                src={avatar}
+                alt="avatar"
+                className="absolute -left-6 w-3 h-3"
+              />
+            </div>
+          </div>
+          <div className="absolute bottom-0 w-full px-5 py-4 space-y-1 ">
+            <h1 className="text-lg bg-gradient-to-r font-semibold text-transparent bg-clip-text from-gray-300 to-lightbule rounded-md">
+              Intelligent Agents to Optimize
+            </h1>
+            <p className="font-extralight text-md">
+              Scale and refine your marketing with AI-powered agents. They track
+              performance in real-time to help you reach more customers
+              efficiently.
+            </p>
+          </div>
+        </FeatureWrapper>
       </div>
     </div>
   );
